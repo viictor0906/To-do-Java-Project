6d7f7615f22c51c6ce6d2e62146e89b6eb7fb612
Víctor <142942997+viictor0906@users.noreply.github.com>
2025-08-29 09:43:01 -0300

    Delete src directory

--- src/main/java/com/ifsc/todo/model/Priorities.java
+++
@@ -1,9 +0,0 @@
-package com.ifsc.todo.model;
-
-//Define which this method could be just one type of the types defined.
-public enum Priorities{
-    LOW,MID,HIGH;
-    private Priorities(){
-        
-    }
-}
